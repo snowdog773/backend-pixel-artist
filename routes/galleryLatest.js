5c8b5475105290603858511fcc33eb2c8c6634e6
Jon Pitans <jonpitans@gmail.com>
2026-03-30 18:04:52 +0100

    add server logs to sql connection

--- routes/galleryLatest.js
+++ routes/galleryLatest.js
@@ -3,12 +3,13 @@ const app = express.Router();
 const asyncMySQL = require("../utils/connection");
 
 app.get("/", async (req, res) => {
+  console.log("Received request for latest gallery items");
   try {
     const results = await asyncMySQL(
       `SELECT Name, Data, userID, Votes, artwork.ID, userdata.Username FROM artwork
       LEFT JOIN userdata ON artwork.userID = userdata.ID
       ORDER BY artwork.Last_edit DESC
-      LIMIT 100`
+      LIMIT 100`,
     );
     res.send(results);
   } catch (error) {
